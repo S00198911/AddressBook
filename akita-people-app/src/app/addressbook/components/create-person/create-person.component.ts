768dd41c26c5d08c101c4d16b4ba44a5363baba2
S00198911 <75222546+S00198911@users.noreply.github.com>
2022-02-22 12:57:07 +0000

    Add files via upload

---
+++ akita-people-app/src/app/addressbook/components/create-person/create-person.component.ts
@@ -0,0 +1,30 @@
+
+import { PeopleService } from '../../services/people.service';
+import { People } from '../../model/people.model';
+import { PeopleStore } from '../../store/people.store';
+import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import * as uuid from 'uuid';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-create-person',
+  templateUrl: 'create-person.component.html'
+})
+export class CreatePersonComponent  {
+
+  createPersonSub: Subscription;
+
+  constructor(private peopleService: PeopleService, private router: Router, private store: PeopleStore) { }
+
+
+  onSubmit(submittedForm: { value: { lastname: any; firstname: any; phone: any; }; invalid: any; }) {
+    if (submittedForm.invalid) {
+      return;
+    }
+    const people: People = {id: uuid.v4(), lastname: submittedForm.value.lastname,firstname: submittedForm.value.firstname, phone: submittedForm.value.phone};
+    this.createPersonSub = this.peopleService.createPerson(people).subscribe(result => {
+      this.router.navigateByUrl('/people');
+    });
+  }
+}
